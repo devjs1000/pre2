621c8834087e4230e31810f45114fcbdc8e7ae03
anand <atim4077@gmail.com>
2022-03-28 02:59:28 +0530

    attribute frame and attributes fram added

--- premon/dom/frames/html.frames.mjs
+++ premon/dom/frames/html.frames.mjs
@@ -5,3 +5,18 @@ const selfFrame = (prexObj) => {
 const nativeFrame = (prexObj) => {
   return `<${prexObj.value}> ${prexObj.value} </${prexObj.value}>`;
 };
+
+const attrFrame = (key, val) => {
+  return `${key} = ${val}`;
+};
+
+const attrsFrame = (attributes) => {
+  const attrBucket=[]
+  for (const key in attributes) {
+    if (Object.hasOwnProperty.call(attributes, key)) {
+      const value = atr[key];
+      attrBucket.push(attrFrame(key, value))
+    }
+  }
+  return attrBucket.join(" ")
+};
